748921d2460df1f5f0475013b68a6daf15c2698a
hiepkoro <hiepxanh@gmail.com>
2018-02-28 14:27:03 +0700

    back

--- src/app/main.js
+++
@@ -1,6 +0,0 @@
-// import { LOCALE_ID } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-// platformBrowserDynamic([{provide: LOCALE_ID, useValue: 'vi'}]).bootstrapModule(AppModule, {providers: [{provide: LOCALE_ID, useValue: 'vi'}]});
-platformBrowserDynamic().bootstrapModule(AppModule);
-//# sourceMappingURL=main.js.map
